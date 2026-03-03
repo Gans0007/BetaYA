7f52cbc2ef3b8083ffaf620d2413a989daea5f9a
Your Ambitions <artemonbagrakov@icloud.com>
2026-03-03 08:27:38 +0200

    api

--- api/main.py
+++ api/main.py
@@ -3,16 +3,17 @@ import hmac
 import hashlib
 import urllib.parse
 import asyncpg
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+app = FastAPI()
 pool = None
-
 
 # ----------------------------
 # Startup
