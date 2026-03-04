3930f7e1f73f8acffd420fd3966f3268390a28d9
Your Ambitions <artemonbagrakov@icloud.com>
2026-03-04 08:57:35 +0200

    new

--- api/main.py
+++ api/main.py
@@ -30,7 +30,7 @@ async def startup():
 # ----------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://youramb.digital"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
