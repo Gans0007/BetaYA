953c2a37436303727ff41353883b07ab927cb5da
Your Ambitions <artemonbagrakov@icloud.com>
2026-03-08 21:34:25 +0200

    new

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,7 +1,7 @@
 import {loadHabitsDashboard} from "./habits.js"
 import {renderWeek} from "./calendar.js"
 import {initNavigation} from "./navigation.js"
-import {renderMe} from "./chat.js"
+import {renderChatUser} from "./chat.js"
 
 const tg = window.Telegram.WebApp
 tg.expand()
@@ -11,6 +11,6 @@ const initData = tg.initData
 renderWeek()
 loadHabitsDashboard(initData)
 
-renderMe()
+renderChatUser()
 
 initNavigation()
