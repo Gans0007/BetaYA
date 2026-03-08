a9832e0c7ba88777fa216805af89274b614e5f5e
Your Ambitions <artemonbagrakov@icloud.com>
2026-03-08 16:14:15 +0200

    new

--- frontend/js/navigation.js
+++ frontend/js/navigation.js
@@ -1,14 +1,36 @@
 export function initNavigation(){
 
 const items = document.querySelectorAll(".nav-item")
+const pages = document.querySelectorAll(".page")
 
 items.forEach(item => {
 
 item.addEventListener("click", () => {
 
-items.forEach(i => i.classList.remove("active"))
+const page = item.dataset.page
 
+/* активная иконка */
+
+items.forEach(i => i.classList.remove("active"))
 item.classList.add("active")
+
+/* переключение страниц */
+
+pages.forEach(p => p.classList.remove("active"))
+
+const activePage = document.getElementById(page + "-page")
+
+if(activePage){
+activePage.classList.add("active")
+}
+
+/* фикс скролла */
+
+const scroll = activePage?.querySelector(".page-content")
+
+if(scroll){
+scroll.scrollTop = 0
+}
 
 })
 
