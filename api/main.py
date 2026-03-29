dc37c2a3d4fb0fb152c8c54e83b5b8fbb2f1dd1c
Your Ambitions <artemonbagrakov@icloud.com>
2026-03-29 16:36:21 +0300

    new

--- api/main.py
+++ api/main.py
@@ -62,7 +62,7 @@ app.add_middleware(
 # ----------------------------
 from api.dashboard import router as dashboard_router
 from api.profile import router as profile_router
-from api.routes.settings import router as settings_router
+from api.settings import router as settings_router
 
 app.include_router(dashboard_router)
 app.include_router(profile_router)
