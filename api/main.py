98d15a56276a7453ffa3d7f2b5c7f46c9c35a9af
Your Ambitions <artemonbagrakov@icloud.com>
2026-04-08 17:06:57 +0300

    new

--- api/main.py
+++ api/main.py
@@ -63,10 +63,12 @@ app.add_middleware(
 from api.dashboard import router as dashboard_router
 from api.profile import router as profile_router
 from api.settings import router as settings_router
+from api.challenges import router as challenges_router
 
 app.include_router(dashboard_router)
 app.include_router(profile_router)
 app.include_router(settings_router)
+app.include_router(challenges_router)
 
 # ----------------------------
 # Test endpoint
